b74af222016edb02a6a24bbb18476423367a7f96
Rick Zamora <richardzamora1952@gmail.com>
2023-04-03 23:28:32 -0500

    Correg unos errore
    con esto ${type.name}-${id}

--- PI-Food-main/client/src/components/Card/Card.jsx
+++ PI-Food-main/client/src/components/Card/Card.jsx
@@ -9,17 +9,22 @@ import { useHistory } from "react-router-dom";
  export function Card({id, name, image, diets, types}) {
  
      const dispatch = useDispatch();
-     const navegate = useHistory();
+     const history = useHistory();
   
-  function handleId(id){
-      dispatch(searchId(id));
-      navegate('/home/id');
-  }
+  async function handleCardClick(id){
+    try {
+        dispatch(searchId(id));
+        history.push("/home/id");
+      } catch (error) {
+        console.error("No hay detalle para esta receta:", error);
+        
+      }
+    }
    return(
     <div>
         
         <div className={CardCss.card}>
-    <img className = {CardCss.img} src={image} alt= 'image' onClick={() => handleId(id)}/>
+    <img className = {CardCss.img} src={image} alt= 'image' onClick={() => handleCardClick(id)}/>
     <h2 className={CardCss.title}>{name}</h2>
     <h5 className={CardCss.diets} > {diets?.map((diet,id) => <span key={`${id}-dieta`}> {diet}, </span> )}</h5>
     <h5 className={CardCss.types}>  {types?.map((dish,id)=> <span key ={`${id}-plato`}> {dish.name ? dish.name : dish}, </span>)}</h5>
--- PI-Food-main/client/src/views/Form/Form.jsx
+++ PI-Food-main/client/src/views/Form/Form.jsx
@@ -1,130 +1,122 @@
-import React from 'react';
+import React from "react";
 
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux'
-import { useHistory } from 'react-router-dom';
-import { getRecipesAll, getTypes, postRecipes } from '../../redux/actions';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getRecipesAll, getTypes, postRecipes } from "../../redux/actions";
 import FormCss from "../Form/Form.module.css";
 
-
-
-function validate(input){
-    let errors = {}
-    if(!input.name) {
-        errors.name = 'Name is require'
-    }
-    if(!input.summary) {
-        errors.summary = 'Summary is require'
-    }
-    return errors
+function validate(input) {
+  let errors = {};
+  if (!input.name) {
+    errors.name = "Name is require";
+  }
+  if (!input.summary) {
+    errors.summary = "Summary is require";
+  }
+  return errors;
 }
 
-
 export default function Form() {
+  const dispatch = useDispatch();
+  //const history = useHistory();
 
-    const dispatch = useDispatch()
-    //const history = useHistory();
-  
-    const type = useSelector((state) => state.types)     
-    const allState = useSelector((state) => state.recipesAll)
-    const [error, setError] = useState({})
-    // console.log(allState);
-    
-  
-    const [input, setInput] = useState({
-        name: '',
-        summary: '',
-        score: 0, 
-        healthScore: 0,
-        image: 'https://st.depositphotos.com/1987177/3470/v/600/depositphotos_34700099-stock-illustration-no-photo-available-or-missing.jpg',
-        steps: '',
-        diets: []
-    })
+  const type = useSelector((state) => state.types);
+  const allState = useSelector((state) => state.recipesAll);
+  const [error, setError] = useState({});
+  // console.log(allState);
 
-    useEffect(() => {
-        dispatch(getTypes())
-     }, [])
+  const [input, setInput] = useState({
+    name: "",
+    summary: "",
+    score: 0,
+    healthScore: 0,
+    image:
+      "https://st.depositphotos.com/1987177/3470/v/600/depositphotos_34700099-stock-illustration-no-photo-available-or-missing.jpg",
+    steps: "",
+    diets: [],
+  });
 
+  useEffect(() => {
+    dispatch(getTypes());
+  }, []);
 
-     function handleDisabled (){
-
-     if((input.name !== '') && (!error.name) && (input.summary !=='')) {
-        return false
-    } return true;
-     }
-
-    async function handleSubmit(evt){
-        evt.preventDefault()
-        dispatch(postRecipes(input))
-        
+  function handleDisabled() {
+    if (input.name !== "" && !error.name && input.summary !== "") {
+      return false;
     }
-   
+    return true;
+  }
 
-    function handleSelect(evt){
-        if(!input.diets.includes(evt.target.value)){
-            setInput({
-                ...input,
-                diets: [...input.diets, evt.target.value]
-            })
-        }
-        console.log(input)
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    dispatch(postRecipes(input));
+  }
+
+  function handleSelect(evt) {
+    if (!input.diets.includes(evt.target.value)) {
+      setInput({
+        ...input,
+        diets: [...input.diets, evt.target.value],
+      });
     }
+    console.log(input);
+  }
 
+  function handleChange(evt) {
+    setInput({
+      ...input,
+      [evt.target.name]: evt.target.value,
+    });
+    setError(
+      validate({
+        ...input,
+        [evt.target.name]: evt.target.value,
+      })
+    );
+    if (
+      allState.find(
+        (recipe) => recipe.name.toLowerCase() === evt.target.value.toLowerCase()
+      )
+    ) {
+      setError({
+        ...input,
+        [evt.target.name]: "Recipe is found",
+      });
+    }
+    console.log(input);
+  }
 
-    function handleChange(evt){
+  function handleNumber(evt) {
+    try {
+      const parsValue = parseInt(evt.target.value);
+      if (Number.isInteger(parsValue) && parsValue >= 0 && parsValue <= 99) {
         setInput({
-            ...input,
-            [evt.target.name]: evt.target.value
-        })
-        setError(validate({
-            ...input,
-            [evt.target.name]:evt.target.value
-        }))
-        if(allState.find(recipe => recipe.name.toLowerCase() === evt.target.value.toLowerCase())){
-            setError({
-                ...input,
-                [evt.target.name]: 'Recipe is found'
-            })
-        }
-        console.log(input)
+          ...input,
+          [evt.target.name]: parsValue,
+        });
+      }
+    } catch {
+      console.log("error");
     }
+    // console.log(input)
+  }
+  //history.push('/home')
 
+  function handleDelete(evt) {
+    setInput({
+      ...input,
+      diets: input.diets.filter((diet) => diet !== evt),
+    });
+  }
 
-    
-    function handleNumber(evt){
-        try{
-            const parsValue = parseInt(evt.target.value)
-            if ((Number.isInteger(parsValue)) && (parsValue >= 0) && (parsValue <= 99)){
-                setInput({
-                    ...input,
-                    [evt.target.name]: parsValue
-                })
-            }
-        }catch{
-            console.log('error')
-        }
-        // console.log(input)
-    }
-    //history.push('/home')
+  console.log(input);
 
-    
-    function handleDelete(evt){
-        setInput({
-            ...input,
-            diets: input.diets.filter(diet => diet !== evt)
-        })
-    }
-
- console.log(input);
-
- return (
+  return (
     <div className={FormCss.contenedor}>
       <div className={FormCss.divgral}>
         <h2 className={FormCss.h2}>CREATE YOUR OWN FOOD </h2>
-        <form
-          className={FormCss.form}
-          onSubmit={(evt) => handleSubmit(evt)}
-        >
+        <form className={FormCss.form} onSubmit={(evt) => handleSubmit(evt)}>
           <div className={FormCss.input}>
             <label>Recipe: </label>
             <input
@@ -137,7 +129,7 @@ export default function Form() {
             />
             {error.name && <p className="error">{error.name}</p>}
           </div>
-  
+
           <div className={FormCss.input}>
             <label> Summary : </label>
             <input
@@ -148,7 +140,7 @@ export default function Form() {
               onChange={(evt) => handleChange(evt)}
             />
           </div>
-  
+
           <div className={FormCss.input}>
             <label> Score: </label>
             <input
@@ -158,7 +150,7 @@ export default function Form() {
               onChange={(evt) => handleNumber(evt)}
             />
           </div>
-  
+
           <div className={FormCss.input}>
             <label> HealthScore: </label>
             <input
@@ -168,7 +160,7 @@ export default function Form() {
               onChange={(evt) => handleNumber(evt)}
             />
           </div>
-  
+
           <div className={FormCss.input}>
             <label> Steps: </label>
             <input
@@ -179,30 +171,42 @@ export default function Form() {
               onChange={(evt) => handleChange(evt)}
             />
           </div>
-  
-          {<button className={FormCss.buttonform} disabled={handleDisabled()}>
-         CREATE!
-         </button>}
-  
+
+          {
+            <button className={FormCss.buttonform} disabled={handleDisabled()}>
+              CREATE!
+            </button>
+          }
+
           <div>
-            <button className={FormCss.buttondelete}  onClick = {(e)=> handleDelete(e) }>X</button>
+            <button
+              className={FormCss.buttondelete}
+              onClick={(e) => handleDelete(e)}
+            >
+              X
+            </button>
             <select
               className={FormCss.input}
               onChange={(evt) => handleSelect(evt)}
             >
               <option disabled>Diets</option>
-              {type?.map((type) => (
-                <option key={type.name} value={type.name}>
+              {type?.map((type,id) => (
+                <option key={`${type.name}-${id}`} value={type.name}>
                   {type.name}
                 </option>
               ))}
             </select>
           </div>
         </form>
-        <a href = '/home'> 
-            <button className={FormCss.buttonform} onClick={()=>dispatch(getRecipesAll())}>Return home</button>
+        <a href="/home">
+          <button
+            className={FormCss.buttonform}
+            onClick={() => dispatch(getRecipesAll())}
+          >
+            Return home
+          </button>
         </a>
       </div>
     </div>
   );
-              }  
+}
